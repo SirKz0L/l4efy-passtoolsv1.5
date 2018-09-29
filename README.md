aeaef3bf398deec1610b8e6312e2417f03b1ad72
Sir.Kz0L <43233009+SirKz0L@users.noreply.github.com>
2018-09-30 05:17:49 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# This PassTools v.1.0 :v
+# CODED : ..:: Sir.Kz0L ::.. a.K.a L4EFY
+
+# Reedit?? Recode??
+# GG Sangad (y)
+
+
+ how to install
+ $get install git
+ $git clone https://github.com/SirKz0L/l4efy.git
+ cd SirKz0L
+ chmod 777 l4efy
+ ./l4efy
+
+
+
+
+    Thanks to : Cyber Familly Association - L4EFY - 007 - Noob Cyber  - HALLU CYBER TEAM - BUFT - ™|BEATDOWN~SYSTEM|™
+                AND ALL ASSOCIATION CYBER IN INDONESIA :)
+                
+Special Thanks To: Allah swt. - My Familly - SULTAN(Clay BURIQUE) - TUANB4DUT - Mr.x_Phantom - BIG COLA 15 Liter - Rokok Ubold, Surya Promild and Sampoerna mild - 25JuL - and my x-Love (AMY)
+
+
+
+# note: i don't know if you using termux, but the order is almost the same :) 
---
+++ install.sh
@@ -0,0 +1,116 @@
+#!/system/usr/xbin/bash
+#!/system/usr/xbin/botbrew
+
+#color
+Lightcyan='\e[96m'
+
+
+apt update
+apt upgrade
+pkg install php wget proot nodejs python python2
+pip2 install lolcat
+pip2 install mechanize
+
+echo -e $Lightcyan " Mohon Di Tunggu "
+	sleep $'1.5'
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/sqlmapproject/sqlmap.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+	echo -e $normal ""
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+	unzip ngrok-stable-linux-arm.zip
+	
+echo -e $Lightcyan" SAMPUN DI JIKUK :v "
+echo -e $normal ""
+git clone https://github.com/mrcakil/Mrcakil.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/asepwew1/MrD.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/hatakecnk/xNot_Found.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/AnonHackerr/toolss.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/2EASY4HACK/TOOLs
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/nmap/nmap.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/rafalgolarz/termux-go
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/sysadmimteam/NetKiller.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/sysadminteam/IPscan.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/Amriez/GreenReaper.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/wpscanteam/wpscan.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/pirmansx/hostcheker.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/ismailtasdelen/Anti-DDOS.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/SkyKnighTeam/IP-TRACK.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/r00t-3xp10it/FakeImageExploiter.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/Manisso/fsociety
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/instabot-py/instabot.py
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv4.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/PowerScript/KatanaFramework.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/paritytech/parity-config-generator.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
+git clone https://github.com/SMH17/bitcoin-hacking-tools.git
+
+echo -e $Lightcyan" SAMPUN DI DONLOT MAS/MBA "
+echo -e $normal ""
